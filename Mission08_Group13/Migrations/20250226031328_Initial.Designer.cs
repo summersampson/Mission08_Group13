02ceeffaee6cf94aeafbd994d862f7d337c76cf8
trey1144 <trey1144@hotmail.com>
2025-02-26 09:53:47 -0700

    fixing migrations

--- Mission08_Group13/Migrations/20250226031328_Initial.Designer.cs
+++ Mission08_Group13/Migrations/20250226031328_Initial.Designer.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Mission08_Group13.Models;
 
 #nullable disable
 
